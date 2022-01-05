204a82a03cbeafdf5578ce317003e74b909040a9
seungdang123 <seungdang123@gmail.com>
2022-01-06 03:47:19 +0900

    Try to fix error

--- app.ts
+++ app.ts
@@ -1,31 +1,31 @@
-interface Store {
+type Store = {
   currentPage: number;
   feeds: NewsFeed[];
-}
+};
 
-interface News {
-  readonly id: number;
-  readonly time_ago: string;
-  readonly title: string;
-  readonly url: string;
-  readonly user: string;
-  readonly content: string;
-}
+type News = {
+  id: number;
+  time_ago: string;
+  title: string;
+  url: string;
+  user: string;
+  content: string;
+};
 
-interface NewsFeed extends News {
-  readonly comments_count: number;
-  readonly points: number;
+type NewsFeed = News & {
+  comments_count: number;
+  points: number;
   read?: boolean;
-}
+};
 
-interface NewsDetail extends News {
-  readonly comments: NewsComment[];
-}
+type NewsDetail = News & {
+  comments: NewsComment[];
+};
 
-interface NewsComment extends News {
-  readonly comments: NewsComment[];
-  readonly level: number;
-}
+type NewsComment = News & {
+  comments: NewsComment[];
+  level: number;
+};
 
 const container: HTMLElement | null = document.getElementById("root");
 const ajax: XMLHttpRequest = new XMLHttpRequest();
@@ -63,29 +63,29 @@ function newsFeed(): void {
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
-      <div class="bg-gray-600 min-h-screen">
-        <div class="bg-white text-xl">
-          <div class="mx-auto px-4">
-            <div class="flex justify-between items-center py-6">
-              <div class="flex justify-start">
-                <h1 class="font-extrabold">Hacker News</h1>
-              </div>
-              <div class="items-center justify-end">
-                <a href="#/page/{{__prev_page__}}" class="text-gray-500">
-                  Previous
-                </a>
-                <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
-                  Next
-                </a>
-              </div>
-            </div> 
-          </div>
-        </div>
-        <div class="p-4 text-2xl text-gray-700">
-          {{__news_feed__}}        
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
         </div>
       </div>
-    `;
+      <div class="p-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
+    </div>
+  `;
 
   if (newsFeed.length === 0) {
     newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
@@ -93,30 +93,28 @@ function newsFeed(): void {
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-        <div class="p-6 ${
-          newsFeed[i].read ? "bg-red-500" : "bg-white"
-        } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
-          <div class="flex">
-            <div class="flex-auto">
-              <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
-            </div>
-            <div class="text-center text-sm">
-              <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
-                newsFeed[i].comments_count
-              }</div>
-            </div>
+      <div class="p-6 ${
+        newsFeed[i].read ? "bg-red-500" : "bg-white"
+      } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
           </div>
-          <div class="flex mt-3">
-            <div class="grid grid-cols-3 text-sm text-gray-500">
-              <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
-              <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
-              <div><i class="far fa-clock mr-1"></i>${
-                newsFeed[i].time_ago
-              }</div>
-            </div>  
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
+              newsFeed[i].comments_count
+            }</div>
           </div>
-        </div>    
-      `);
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>    
+    `);
   }
 
   template = template.replace("{{__news_feed__}}", newsList.join(""));
@@ -136,33 +134,33 @@ function newsDetail(): void {
   const id = location.hash.substr(7);
   const newsContent = getData<NewsDetail>(CONTENT_URL.replace("@id", id));
   let template = `
-      <div class="bg-gray-600 min-h-screen pb-8">
-        <div class="bg-white text-xl">
-          <div class="mx-auto px-4">
-            <div class="flex justify-between items-center py-6">
-              <div class="flex justify-start">
-                <h1 class="font-extrabold">Hacker News</h1>
-              </div>
-              <div class="items-center justify-end">
-                <a href="#/page/${store.currentPage}" class="text-gray-500">
-                  <i class="fa fa-times"></i>
-                </a>
-              </div>
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
             </div>
           </div>
         </div>
-  
-        <div class="h-full border rounded-xl bg-white m-6 p-4 ">
-          <h2>${newsContent.title}</h2>
-          <div class="text-gray-400 h-20">
-            ${newsContent.content}
-          </div>
-  
-          {{__comments__}}
-  
-        </div>
       </div>
-    `;
+
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div>
+  `;
 
   for (let i = 0; i < store.feeds.length; i++) {
     if (store.feeds[i].id === Number(id)) {
@@ -183,14 +181,14 @@ function makeComment(comments: NewsComment[]): string {
     const comment: NewsComment = comments[i];
 
     commentString.push(`
-        <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
-          <div class="text-gray-400">
-            <i class="fa fa-sort-up mr-2"></i>
-            <strong>${comment.user}</strong> ${comment.time_ago}
-          </div>
-          <p class="text-gray-700">${comment.content}</p>
-        </div>      
-      `);
+      <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+        <div class="text-gray-400">
+          <i class="fa fa-sort-up mr-2"></i>
+          <strong>${comment.user}</strong> ${comment.time_ago}
+        </div>
+        <p class="text-gray-700">${comment.content}</p>
+      </div>      
+    `);
 
     if (comment.comments.length > 0) {
       commentString.push(makeComment(comment.comments));
